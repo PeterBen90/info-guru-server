2e0d186516ed93b3829e3addae143bbb874761b0
Peter Benvenuto <peterbenvenuto90@yahoo.ca>
2018-06-12 11:41:41 -0400

    google oath stores user in remote database

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 const express = require('express');
-require('./services/passport');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+const { User } = require('./models/user');
+const { Passport } = require('./services/passport');
+
+
+mongoose.connect(keys.mongoURI);
 
 
 const app = express();
--- services/passport.js
+++ services/passport.js
@@ -1,8 +1,10 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
+const { User } = require('../models/user');
 const keys = require('../config/keys');
 
-passport.use(
+const Passport = passport.use(
   new GoogleStrategy(
     {
       clientID: keys.googleClientID,
@@ -10,9 +12,9 @@ passport.use(
       callbackURL: '/auth/google/callback'
     },
     (accessToken, refreshToken, profile, done) => {
-      console.log('accessToken', accessToken);
-      console.log('refreshToken', refreshToken);
-      console.log('profile:', profile);
+      new User({ googleId: profile.id }).save();
     }
   )
 );
+
+module.exports = { Passport };
