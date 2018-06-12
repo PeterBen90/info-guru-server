4e9bcf6775de92e1f5464c23464bc193c6896dbf
Peter Benvenuto <peterbenvenuto90@yahoo.ca>
2018-06-12 14:11:43 -0400

    completed google oauth flow

--- models/user.js
+++ models/user.js
@@ -1,10 +1,10 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
   googleId: String
 });
 
-const User = mongoose.model('Users', userSchema);
+const User = mongoose.model("Users", userSchema);
 
 module.exports = { User };
--- services/passport.js
+++ services/passport.js
@@ -1,18 +1,38 @@
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const mongoose = require('mongoose');
-const { User } = require('../models/user');
-const keys = require('../config/keys');
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const mongoose = require("mongoose");
+const { User } = require("../models/user");
+const keys = require("../config/keys");
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(user => {
+    done(null, user);
+  });
+});
 
 const Passport = passport.use(
   new GoogleStrategy(
     {
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
-      callbackURL: '/auth/google/callback'
+      callbackURL: "/auth/google/callback"
     },
     (accessToken, refreshToken, profile, done) => {
-      new User({ googleId: profile.id }).save();
+      User.findOne({ googleId: profile.id }).then(existingUser => {
+        if (existingUser) {
+          //already have record with given profile ID
+          done(null, existingUser);
+        } else {
+          //we don't have a user with this ID, make a new record
+          new User({ googleId: profile.id })
+            .save()
+            .then(user => done(null, existingUser));
+        }
+      });
     }
   )
 );
