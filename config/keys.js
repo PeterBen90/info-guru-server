d4a03861062f17336bf818b692f8eba7276cd35e
Peter Benvenuto <peterbenvenuto90@yahoo.ca>
2018-06-14 11:34:51 -0400

    deploy to heroku

--- config/keys.js
+++ config/keys.js
@@ -2,7 +2,7 @@
 
 if (process.env.NODE_ENV === 'production') {
   // in production, return production keys
-  module.exports = rquire('./prod');
+  module.exports = require('./prod');
 } else {
   // in devlopment, return dev keys
   module.exports = require('./dev');
